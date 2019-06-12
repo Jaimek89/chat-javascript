d823879e9e075c904ca9aa0a55cfc65a59978dc7
Jaimek89 <jaimek89@gmail.com>
2019-06-12 18:40:08 +0100

    removed mlab server

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 const io = socketio.listen(server)
 
 // db connection
-mongoose.connect('mongodb://jaimek:jaimek123@ds237735.mlab.com:37735/chat-javascript-jaimek89', {
+mongoose.connect('mongodb://localhost/chat', {
     useNewUrlParser: true
 })
     .then(db => console.log('db is connected'))
