e43266ca49dea3e804c61f4e7fba9121225b4c49
Jaimek89 <jaimek89@gmail.com>
2019-06-12 10:52:41 +0100

    added login interface

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -6,6 +6,30 @@ $(function () {
     const $messageForm = $('#message-form')
     const $messageBox = $('#message')
     const $chat = $('#chat')
+
+    // obtaining DOM elements from the nicknameForm
+    const $nickForm = $('#nickForm')
+    const $nickError = $('#nickError')
+    const $nickname = $('#nickname')
+
+    const $users = $('#usernames')
+
+    $nickForm.submit(e => {
+        e.preventDefault()
+        socket.emit('new user', $nickname.val(), data => {
+            if (data) {
+                $('#nickWrap').hide()
+                $('#contentWrap').show()
+            } else {
+                $nickError.html(`
+                    <div class="alert alert-danger mt-2">
+                        That username is logged already.
+                    </div>
+                `)
+            }
+            $nickname.val('')
+        })
+    })
     
     // events
     $messageForm.submit( e => {
--- src/sockets.js
+++ src/sockets.js
@@ -1,9 +1,24 @@
 module.exports = function (io) {
+
+    let nicknames = []
+
     io.on('connection', socket => {
         console.log('new user connected')
+
+        socket.on('new user', (data, cb) => {
+            if(nicknames.indexOf(data) != -1) {
+                cb(false)
+            } else {
+                cb(true)
+                socket.nickname = data
+                nicknames.push(socket.nickname)
+            }
+        })
 
         socket.on('send message', function (data) {
             io.sockets.emit('new message', data)
         })
+
+
     })
 }
