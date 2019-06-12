8957193a6d5a2b5e36bf22e8068443d43eb73f69
Jaimek89 <jaimek89@gmail.com>
2019-06-12 16:18:53 +0100

    added whisper and errors

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -34,7 +34,9 @@ $(function () {
     // events
     $messageForm.submit( e => {
         e.preventDefault()
-        socket.emit('send message', $messageBox.val())
+        socket.emit('send message', $messageBox.val(), data => {
+            $chat.append(`<p class="error">${data}</p>`)
+        })
         $messageBox.val('')
     })
 
@@ -48,5 +50,9 @@ $(function () {
             html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`
         }
         $users.html(html)
+    })
+
+    socket.on('whisper', data => {
+        $chat.append(`<p class="whisper"><b>${data.nick}:</b> ${data.msg}</p>`)
     })
 })
